caf1c8a29108271aa6219dc4ca6813763eda3883
J-dar Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2021-09-07 00:07:00 +0800

    027.a:  - Prepend Errors types on error messages.

--- errors.js
+++ errors.js
@@ -1,10 +1,10 @@
 function ParseFailureError(message = 'Parse configuration file failed.') {
-    this.message = message;
+    this.message = `ParseFailureError : ${message}`;
     return this;
 }
 
 function UnknownFileFormatError(message = 'cannot natively parse file format, please pass a custom parser function(string) => JSON') {
-    this.message = message;
+    this.message = `UnknownFileFormatError : ${message}`;
     return this;
 }
 
